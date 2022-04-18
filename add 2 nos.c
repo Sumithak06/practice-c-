3bec9b080b8d43c652cc3aa5987a85af30cd0c9e
just_code <98280286+Sumithak06@users.noreply.github.com>
2022-04-18 20:36:16 +0530

    Add files via upload

---
+++ add 2 nos.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf(" enter number 1:");
+	scanf("%d",&a);
+	printf(" enter number 2:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("ans is %d",c);
+	return 0;
+}
